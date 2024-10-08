d8b149d4a8e2080863ca3b9f8766ae36d95eb828
TanChu <maildehoconline@gmail.com>
2024-10-08 14:43:02 +0700

    Init Project

---
+++ src/main/java/com/airline/airline_booking/AirlineBookingApplication.java
@@ -0,0 +1,13 @@
+package com.airline.airline_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineBookingApplication.class, args);
+	}
+
+}
